de7ca4bba8e18234a07e734e35f3c9e4ecdb3b12
unicorn-factory <unicornfactorygames@gmail.com>
2018-01-16 23:38:47 +0000

    Main Menu e Pausa
    Backbone do main menu e do "sistema de pausa".

---
+++ BackBone/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+    const int MAINMENU  = 0;
+    const int LEVEL1    = 1;
+    const int CREDITS   = 2;
+
+    public void GoToMainMenu()
+    {
+        SceneManager.LoadScene(MAINMENU);
+    }
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(LEVEL1);
+    }
+
+    public void ShowCredits()
+    {
+        SceneManager.LoadScene(CREDITS);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
---
+++ BackBone/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour {
+
+    public bool IsPaused { get; set; }
+
+    private void Start()
+    {
+        IsPaused = false;
+        gameObject.SetActive(IsPaused);
+    }
+
+    public void PauseGame()
+    {
+        IsPaused = true;
+        gameObject.SetActive(IsPaused);
+    }
+
+    public void ResumeGame()
+    {
+        IsPaused = false;
+        gameObject.SetActive(IsPaused);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+}
